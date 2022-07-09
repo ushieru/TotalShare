7696af357e0da1013a9fd5f58fb881c92e930f8d
ushieru <uzielcocolan@gmail.com>
2022-07-09 18:32:01 -0500

    Add path.join Fix File(filePath).writeAsBytesSync performance Add separated snackBottom function

--- lib/controller/server_socket_controller.dart
+++ lib/controller/server_socket_controller.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart' as path;
 import 'package:get/get.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:total_share/bin/wrap_server_socket.dart';
@@ -7,22 +8,21 @@ import 'package:total_share/bin/wrap_server_socket.dart';
 class ServerSocketController extends GetxController {
   RxString address = ''.obs;
   RxMap<String, Socket> connectedSockets = <String, Socket>{}.obs;
-  RxList<MapEntry<String, Socket>> connectedSocketsList =
-      <MapEntry<String, Socket>>[].obs;
+  String appDirectory = '';
 
   @override
   void onInit() {
     getApplicationDocumentsDirectory().then((appDocDir) {
-      String appDocPath = appDocDir.path;
-      final directory = Directory(appDocPath + '\\TotalShare');
+      appDirectory = path.join(appDocDir.path, 'TotalShare');
+      final directory = Directory(appDirectory);
       if (!directory.existsSync()) directory.create();
     });
+
     WrapServerSocket.create().then((wrapServerSocket) {
       address.value = wrapServerSocket.address;
       wrapServerSocket
         ..addHandler('register', (socket, payload, {meta}) async {
           final clientName = String.fromCharCodes(payload);
-          debugPrint(clientName);
           connectedSockets[clientName] = socket;
           Get.snackbar(
             'New Connecion 📡',
@@ -31,23 +31,22 @@ class ServerSocketController extends GetxController {
             duration: const Duration(seconds: 2),
           );
         })
-        ..addHandler('file', (socket, payload, {meta}) async {
-          Directory appDocDir = await getApplicationDocumentsDirectory();
-          String appDocPath = appDocDir.path;
-          await File(
-                  appDocPath + '\\TotalShare\\${meta['name']}.${meta['ext']}')
-              .writeAsBytes(payload, mode: FileMode.append);
-          Get.snackbar(
-            'New Download 💾',
-            '${meta['name']}.${meta['ext']}',
-            snackPosition: SnackPosition.BOTTOM,
-            margin: const EdgeInsets.only(bottom: 10, left: 30, right: 30),
-            duration: const Duration(seconds: 2),
-          );
+        ..addHandler('file', (_socket, payload, {meta}) async {
+          final String fileName = '${meta['name']}.${meta['ext']}';
+          final String filePath = path.join(appDirectory, fileName);
+          File(filePath).writeAsBytesSync(payload, mode: FileMode.append);
         });
     });
-    connectedSockets.listen((connectedSockets) =>
-        connectedSocketsList.value = connectedSockets.entries.toList());
     super.onInit();
+  }
+
+  void snackBottom(String title, String message) {
+    Get.snackbar(
+      title,
+      message,
+      snackPosition: SnackPosition.BOTTOM,
+      margin: const EdgeInsets.only(bottom: 10, left: 30, right: 30),
+      duration: const Duration(seconds: 2),
+    );
   }
 }
